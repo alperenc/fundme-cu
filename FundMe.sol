cb87c402b8f3a5493227637bf699efef70ec8e0c
alperenc <alperenc@gmail.com>
2024-08-26 19:09:08 +0300

    feat: Add funders & refactor: Move getPrice() and getConversionRate(uint) into PriceConverter.sol

--- FundMe.sol
+++ FundMe.sol
@@ -1,26 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-import { AggregatorV3Interface } from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import {PriceConverter} from "./PriceConverter.sol";
 
 contract FundMe {
+    using PriceConverter for uint256;
 
     uint256 public minimumUsd = 5e18;
 
+    address[] public funders;
+    mapping(address funder => uint256 amountFunded) public addressToAmoundFunded;
+
     function fund() public payable {
         // Allow users to send $ (have a minimum $ sent)
-        require(getConversionRate(msg.value) > minimumUsd, "Not enough USD sent");
-    }
-
-    function getPrice() public view returns (uint256) {
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-        ( , int256 price, , , ) = priceFeed.latestRoundData(); // Price of ETH in USD (8 decimals)
-        return uint256(price) * 1e10;
-    }
-
-    function getConversionRate(uint256 ethAmount) public view returns (uint256) {
-        uint256 ethPrice = getPrice();
-        uint256 ethAmountConverted = (ethAmount * ethPrice) / 1e18; // Convert ETH to USD and then USD to ETH
-        return ethAmountConverted;
+        require(msg.value.getConversionRate() > minimumUsd, "Not enough USD sent");
+        funders.push(msg.sender);
+        addressToAmoundFunded[msg.sender] += msg.value;
     }
 }
