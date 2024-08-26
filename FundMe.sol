c537e2d4cc809947ecba2c743862e601dfc26100
alperenc <alperenc@gmail.com>
2024-08-26 18:41:53 +0300

    feat: Get real world price data of ETH/USD from Chainlink

--- FundMe.sol
+++ FundMe.sol
@@ -1,9 +1,26 @@
 // SPDX-License-Identifier: MIT
-pragma solidity *0.8.18;
+pragma solidity ^0.8.18;
+
+import { AggregatorV3Interface } from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
 contract FundMe {
+
+    uint256 public minimumUsd = 5e18;
+
     function fund() public payable {
         // Allow users to send $ (have a minimum $ sent)
-        require(msg.value > 1e18, "Not enough ETH sent"); // 1e18 = 1 ETH = 10^9 gwei = 10^18 wei
+        require(getConversionRate(msg.value) > minimumUsd, "Not enough USD sent");
+    }
+
+    function getPrice() public view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        ( , int256 price, , , ) = priceFeed.latestRoundData(); // Price of ETH in USD (8 decimals)
+        return uint256(price) * 1e10;
+    }
+
+    function getConversionRate(uint256 ethAmount) public view returns (uint256) {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountConverted = (ethAmount * ethPrice) / 1e18; // Convert ETH to USD and then USD to ETH
+        return ethAmountConverted;
     }
 }
